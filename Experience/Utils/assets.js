ed29ef75fe7e6cee8c353b8c5d6091060b758658
mimitamara <108095783+mimitamara@users.noreply.github.com>
2024-01-16 19:27:29 +0100

    Add files via upload

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: "room",
+    type: "glbModel",
+    path: "/models/final_woFloor.glb",
+  },
+  {
+    name: "screen",
+    type: "videoTexture",
+    path: "/textures/VideoFriends.mp4",
+  },
+];
---
+++ Experience/World/Room.js
@@ -0,0 +1,23 @@
+import * as THREE from "three";
+import Experience from "../Experience.js";
+
+export default class Room {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+    this.time = this.experience.time;
+    this.room = this.resources.items.room;
+    this.actualRoom = this.room.scene;
+
+    this.setModel();
+  }
+
+  setModel() {
+    this.scene.add(this.actualRoom);
+  }
+
+  resize() {}
+
+  update() {}
+}
